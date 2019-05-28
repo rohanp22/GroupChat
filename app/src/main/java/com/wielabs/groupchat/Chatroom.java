dd445a9727a65c8e6106b196bf2856657ff11e85
Rohan <rohanpandey2210@gmail.com>
2019-05-28 15:57:17 +0530

    Initial commit

---
+++ app/src/main/java/com/wielabs/groupchat/Chatroom.java
@@ -0,0 +1,143 @@
+package com.wielabs.groupchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class Chatroom extends AppCompatActivity {
+    EditText e1;
+    TextView t1;
+
+    private String user_name,room_name;
+
+    DatabaseReference reference;
+    String temp_key;
+    ListView l;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        setContentView(R.layout.activity_chatroom);
+        e1= (EditText)findViewById(R.id.editText2);
+        //t1= (TextView)findViewById(R.id.textView);
+        l = (ListView) findViewById(R.id.listView2);
+        arrayList = new ArrayList<>();
+
+        if(getSupportActionBar()!=null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+
+        user_name = getIntent().getExtras().get("user_name").toString();
+        room_name = getIntent().getExtras().get("room_name").toString();
+        reference = FirebaseDatabase.getInstance().getReference().child(room_name);
+        setTitle(" Room - "+room_name);
+
+
+
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                append_chat(dataSnapshot);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                append_chat(dataSnapshot);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                append_chat(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        arrayList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        l.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+            finish();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void send(View v)
+    {
+        Map<String,Object> map = new HashMap<String,Object>();
+        temp_key = reference.push().getKey();
+        reference.updateChildren(map);
+
+        DatabaseReference child_ref = reference.child(temp_key);
+        Map<String,Object> map2 = new HashMap<>();
+        map2.put("name",user_name);
+        map2.put("msg", e1.getText().toString());
+        child_ref.updateChildren(map2).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        e1.setText("");
+
+    }
+    public void append_chat(DataSnapshot ss)
+    {
+
+        String chat_msg,chat_username;
+        Iterator i = ss.getChildren().iterator();
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        Set<String> set = new HashSet<String>();
+
+        while(i.hasNext())
+        {
+            chat_msg = ((DataSnapshot)i.next()).getValue().toString();
+            chat_username = ((DataSnapshot)i.next()).getValue().toString();
+            Log.d("msg",chat_msg+chat_username);
+            arrayList.add(chat_username + ": " +chat_msg);
+            adapter.notifyDataSetChanged();
+        }
+        l.setAdapter(adapter);
+        l.setSelection(l.getAdapter().getCount()-1);
+    }
+}
